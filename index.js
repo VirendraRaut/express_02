7f8f318bff37f292c70d7f3b67367b4d463ee6e5
VirendraRaut <virendraraut1306@gmail.com>
2025-10-23 11:22:59 +0530

    dotenv

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-const port = 3002;
+const port = process.env.PORT || 3002;
 
 app.get("/", (req, res) => {
   res.send("Hello World..!");
